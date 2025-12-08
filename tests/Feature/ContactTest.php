c7d3cc50a527a9c32509089e8fa6e8596d42d8b9
菅原　祥宏 <sugawara@sugawarasachihiroshinoMacBook-Air.local>
2025-12-08 19:45:07 +0900

    原因は、php artisan migrate:freshというコマンドのうち忘れでした。あと、データベースの空のテスト追加しました。

--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -1,7 +1,11 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\User;
 use App\Models\Company;
+use App\Models\Contact;
+
+pest()->use(RefreshDatabase::class);
 
 beforeEach(function () {
     $this->user = User::factory()->create();
@@ -14,3 +18,9 @@ test('連絡先一覧のテスト', function () {
     $response->assertStatus(200);
 });
 
+test('連絡先のエラー確認', function() {
+    $response = $this->actingAs($this->user)
+        ->get('/contacts');
+
+    $this->assertDatabaseEmpty('contacts');
+});
