3fcc613727812dd15a7ec12ba80becb2af3c4ef9
菅原　祥宏 <sugawara@sugawarasachihiroshinoMacBook-Air.local>
2026-01-23 17:18:21 +0900

    apiのaをAにしました。

--- app/Http/Controllers/apiContactController.php
+++ app/Http/Controllers/apiContactController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class apiContactController extends Controller
+class ApiContactController extends Controller
 {
     //
     public function getContactsByCompany($companyId) {
