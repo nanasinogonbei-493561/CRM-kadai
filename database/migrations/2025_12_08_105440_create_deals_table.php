64a0de09dc7c246e1a9f552f524ec73df0e9c45d
菅原　祥宏 <sugawara@sugawarasachihiroshinoMacBook-Air.local>
2026-01-23 17:22:15 +0900

    contact_idを追加し忘れたので

--- database/migrations/2025_12_08_105440_create_deals_table.php
+++ database/migrations/2025_12_08_105440_create_deals_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('deals', function (Blueprint $table) {
             $table->id();
             $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            $table->foreignId('contact_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->integer('amount');
